fb448f306eb6c74f6d18b19f9e548f82f1e35e0d
joellemaximin <joelle.maximin@gmail.com>
2019-10-13 19:17:46 +0200

    show event article working

--- src/js/load.js
+++ src/js/load.js
@@ -1,1 +1,9 @@
-const 
+
+function show(elem) {
+    var paragraph = elem.querySelector(".collapse");
+    if (paragraph.style.display == "none") {
+        paragraph.style.display = "block";
+    } else {
+        paragraph.style.display = "none";
+    }
+}
