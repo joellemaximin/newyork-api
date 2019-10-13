630081f32e3de87462ef5d2e91400757e1420793
joellemaximin <joelle.maximin@gmail.com>
2019-10-14 01:54:33 +0200

    update package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,75 @@
 // import all task from gulp
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
-var uglify = require('gulp-uglify');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const { series } = require('gulp');
 
 gulp.task('msg', function(){
     return console.log('gulp tourne ..')
 });
 
-gulp.task('fileHTML', function(){
+function fileHTML(){
     gulp.src('src/*.html')
-        .pipe(gulp.dest('dist'))
-})
+		.pipe(gulp.dest('./html'))
+		.pipe(browserSync.stream());
 
-gulp.task('image', () =>
-	gulp.src('src/images/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('dist/images'))
-);
+}
 
-gulp.task('minify', () =>
+function minify(){
 	gulp.src('src/js/*.js')
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/js'))
-);
+	.pipe(uglify())
+	.pipe(gulp.dest('./js'))
+
+};
+
+function style(){
+	gulp.src('./src/scss/**/*.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('./css/'))
+	.pipe(browserSync.stream());
+
+};
+
+// gulp.task('clean', () => {
+//     return del([
+//         'css/list.css',
+//     ]);
+// });
+
+function watch(){
+	browserSync.init({
+		server: {
+			baseDir: './src/'
+		}
+	});
+	
+	gulp.watch('./src/scss/**/*scss', style);
+	gulp.watch('/src/js/**/*.js').on('change', browserSync.reload);
+	// gulp.watch('src/images/*').on('change', browserSync.reload);
+	gulp.watch('/src/*.html').on('change', browserSync.reload);
+
+}
+
+// gulp.task('default', function() {
+//     gulp.watch('src/*.html', gulp.series('fileHTML')),
+//     gulp.watch('src/images/*', gulp.series('images')),
+//     gulp.watch('src/js/*.js', gulp.series('minify')),
+//     gulp.watch('src/sass/*.scss', gulp.series('sass')),
+//     return
+// });
+
+// gulp.task('clean:', function (resolve) {
+//     del.sync('');
+//     resolve();
+// })
+
+// gulp.task('default', async function(){ gulp.series('sass','images','fileHTML','minify' )})
+
+gulp.task('default', gulp.series(minify, style, fileHTML));
+
+// exports.build = build;
+exports.style = style;
+exports.minify = minify;
+exports.watch = watch;
